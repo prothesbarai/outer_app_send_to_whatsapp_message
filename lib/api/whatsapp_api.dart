2c191d8d7d1394cc80f10eafc3e80a3b094d368f
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-12 11:58:42 +0600

    asp v99

--- lib/api/whatsapp_api.dart
+++ lib/api/whatsapp_api.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class WhatsappApi {
-  final String accessToken = "EAALvlPgeNEIBPyf3ymhHn63xihunLoRbSZA34Slqii4ZBdIZCZAXY8ukZCZBBZA1AHWRVbPhzIGgUCd84DVMWuiq7ZBOdQKugOUSazQcqZA4QihJOhQ4iuCTg2Qa5LhDp7me0BofxSNtAB6a5prQoZCVmjWoMV0STOZCFzdNl5l8FIylYrEjIYB5zL2YoqodtINygZDZD";
-  final String phoneNumberId = "893804690476575";
+  final String accessPermanentToken = "EAALvlPgeNEIBPyf3ymhHn63xihunLoRbSZA34Slqii4ZBdIZCZAXY8ukZCZBBZA1AHWRVbPhzIGgUCd84DVMWuiq7ZBOdQKugOUSazQcqZA4QihJOhQ4iuCTg2Qa5LhDp7me0BofxSNtAB6a5prQoZCVmjWoMV0STOZCFzdNl5l8FIylYrEjIYB5zL2YoqodtINygZDZD";
+  final String fromPhoneNumberId = "893804690476575";
 
   Future<void> sendText(String receiverPhnNo, String message) async {
-    final url = Uri.parse("https://graph.facebook.com/v20.0/$phoneNumberId/messages");
+    final url = Uri.parse("https://graph.facebook.com/v20.0/$fromPhoneNumberId/messages");
 
     /*final data = {
       "messaging_product": "whatsapp",
@@ -42,7 +42,7 @@ class WhatsappApi {
     try {
       final response = await http.post(
         url,
-        headers: {"Authorization": "Bearer $accessToken", "Content-Type": "application/json",},
+        headers: {"Authorization": "Bearer $accessPermanentToken", "Content-Type": "application/json",},
         body: jsonEncode(data),
       );
 
