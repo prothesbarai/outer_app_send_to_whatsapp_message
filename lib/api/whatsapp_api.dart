21f302d1bd3a3126381297fbe18e77fa1b933fec
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-12 12:31:57 +0600

    asp

--- lib/api/whatsapp_api.dart
+++ lib/api/whatsapp_api.dart
@@ -6,12 +6,13 @@ class WhatsappApi {
   final String accessPermanentToken = "EAALvlPgeNEIBPyf3ymhHn63xihunLoRbSZA34Slqii4ZBdIZCZAXY8ukZCZBBZA1AHWRVbPhzIGgUCd84DVMWuiq7ZBOdQKugOUSazQcqZA4QihJOhQ4iuCTg2Qa5LhDp7me0BofxSNtAB6a5prQoZCVmjWoMV0STOZCFzdNl5l8FIylYrEjIYB5zL2YoqodtINygZDZD";
   final String fromPhoneNumberId = "893804690476575";
 
-  Future<void> sendText(String receiverPhnNo, String message) async {
+  Future<void> sendText(String receiverPhnNo,String name,String order,String price) async {
     final url = Uri.parse("https://graph.facebook.com/v22.0/$fromPhoneNumberId/messages");
 
-    /*final data = {
+    /// >>> Template Format (Limitation -> 30-60 Char And Pre-approved)
+    final data = {
       "messaging_product": "whatsapp",
-      "to": "+88$receiverPhnNo",
+      "to": receiverPhnNo,
       "type": "template",
       "template": {
         "name": "first_template",
@@ -20,22 +21,25 @@ class WhatsappApi {
           {
             "type": "body",
             "parameters": [
-              {"type": "text", "text": "Prothes"},
-              {"type": "text", "text": message},
+              {"type": "text", "text": name},
+              {"type": "text", "text": receiverPhnNo},
+              {"type": "text", "text": order},
+              {"type": "text", "text": price},
             ]
           }
         ]
       }
-    };*/
+    };
 
-    final data = {
+    /// >>> Simple Format (Limitation -> 400-500 Char And Required for cold messages)
+    /*final data = {
       "messaging_product": "whatsapp",
       "to": receiverPhnNo,
       "type": "text",
       "text": {
         "body": "এইখানে তোমার 500 character পর্যন্ত message লিখতে পারবে।"
       }
-    };
+    };*/
 
 
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
         child: ElevatedButton(
           onPressed: () async {
             try {
-              await whatsAppApi.sendText("+8801317818826", "Hello from Flutter!",);
+              await whatsAppApi.sendText("8801317818826", "Prothes Barai","Computer 1pics,Mobile 1 pics","100000");
             } catch (e) {
               debugPrint("Some Error $e");
             }
