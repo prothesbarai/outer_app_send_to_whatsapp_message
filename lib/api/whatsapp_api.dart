e7b868fe971299930b9039385a28b003ce69d5fc
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-13 15:41:16 +0600

    asp v0099

--- lib/api/whatsapp_api.dart
+++ lib/api/whatsapp_api.dart
@@ -3,12 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class WhatsappApi {
-  final String accessPermanentToken = "EAALvlPgeNEIBPyf3ymhHn63xihunLoRbSZA34Slqii4ZBdIZCZAXY8ukZCZBBZA1AHWRVbPhzIGgUCd84DVMWuiq7ZBOdQKugOUSazQcqZA4QihJOhQ4iuCTg2Qa5LhDp7me0BofxSNtAB6a5prQoZCVmjWoMV0STOZCFzdNl5l8FIylYrEjIYB5zL2YoqodtINygZDZD";
-  final String fromPhoneNumberId = "893804690476575";
+  static final String accessPermanentToken = "EAALvlPgeNEIBPyf3ymhHn63xihunLoRbSZA34Slqii4ZBdIZCZAXY8ukZCZBBZA1AHWRVbPhzIGgUCd84DVMWuiq7ZBOdQKugOUSazQcqZA4QihJOhQ4iuCTg2Qa5LhDp7me0BofxSNtAB6a5prQoZCVmjWoMV0STOZCFzdNl5l8FIylYrEjIYB5zL2YoqodtINygZDZD";
+  static final String fromPhoneNumberId = "893804690476575";
 
-  Future<void> sendText(String receiverPhnNo,String name,String order,String price) async {
+  static Future<Map<String,dynamic>?> sendText(String receiverPhnNo,String name,String order,String price) async {
     final url = Uri.parse("https://graph.facebook.com/v22.0/$fromPhoneNumberId/messages");
-
     /// >>> Template Format (Limitation -> 30-60 Char And Pre-approved)
     /*final data = {
       "messaging_product": "whatsapp",
@@ -30,7 +29,6 @@ class WhatsappApi {
         ]
       }
     };*/
-
     /// >>> Simple Format (Limitation -> 400-500 Char And Required for cold messages)
     final data = {
       "messaging_product": "whatsapp",
@@ -85,13 +83,16 @@ class WhatsappApi {
       );
 
       if (response.statusCode == 200) {
-        if (kDebugMode) {print("Message sent : ${response.body}");}
+        final result = jsonDecode(response.body);
+        if (kDebugMode) {print("$result");}
+        return result;
       } else {
         if (kDebugMode) {print("Failed : ${response.statusCode} ${response.body}");}
+        return null;
       }
     } catch (e) {
       if (kDebugMode) {print("Error sending message: $e");}
-      rethrow;
     }
+    return null;
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,79 @@
 import 'package:flutter/material.dart';
 import '../api/whatsapp_api.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  bool isLoading = false;
+
+
+  void showDialogue(String title, String message){
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [ElevatedButton(onPressed: ()=>Navigator.pop(context), child: Text("OK"))],
+        ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final whatsAppApi = WhatsappApi();
+
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              await whatsAppApi.sendText("8801718535601", "Prothes Barai","Computer 1pics,Mobile 1 pics","100000");
-            } catch (e) {
-              debugPrint("Some Error $e");
-            }
-          },
-          child: const Text("Send WhatsApp Message"),
-        ),
+      appBar: AppBar(title: const Text("Home"),),
+      body: Stack(
+        children: [
+
+          Center(
+            child: ElevatedButton(
+              onPressed: isLoading ? null : () async {
+                try {
+                  setState(() {isLoading = true;});
+                  final result = await WhatsappApi.sendText("8801718535601", "Prothes Barai","Computer 1pics,Mobile 1 pics","100000");
+                  setState(() {isLoading = false;});
+                  if(result != null){
+                    showDialogue("Successful","Your Message Successfully Send.");
+                  }else{
+                    showDialogue("Failed","Something went wrong. Please Try again!");
+                  }
+                } catch (e) {
+                  debugPrint("Some Error $e");
+                }
+              },
+              child: Text(isLoading ? "Sending" : "Send WhatsApp Message"),
+            ),
+          ),
+          if(isLoading)
+            Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(color: Colors.black.withValues(alpha: 0.6), borderRadius: BorderRadius.circular(12),),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircularProgressIndicator(color: Colors.white),
+                    SizedBox(height: 15),
+                    Text("Loading...", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold,),),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
